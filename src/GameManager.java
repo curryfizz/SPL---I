bd99f8532194bfa0de033ff063bc08f35707ff37
AyeshaAfrozaMohsin <whiteheadiceprince@gmail.com>
2022-09-22 21:30:37 +0600

    test commit

--- src/GameManager.java
+++ src/GameManager.java
@@ -4,22 +4,13 @@ import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class GameManager {
+
     UI ui = new UI(this);
     public static void main(String[] args) throws InterruptedException{
         new GameManager();
-
-        while(true == true)
-        {
-            TimeUnit.SECONDS.sleep(1/2);
-            double mouseX = MouseInfo.getPointerInfo().getLocation().getX();
-            double mouseY = MouseInfo.getPointerInfo().getLocation().getY();
-            System.out.println("X:" + mouseX);
-            System.out.println("Y:" + mouseY);
-            //make sure to import
-        }
     }
 
     public GameManager(){
-
+        GameTimer gameTimer = new GameTimer(ui);
     }
 }
---
+++ src/GameTimer.java
@@ -0,0 +1,82 @@
+package src;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameTimer {
+    UI ui;
+    JPanel backGroundPanel;
+    Timer timer;
+
+    Font font;
+    JLabel counterLabel;
+    int second;
+    int minute;
+
+    public GameTimer(UI ui) {
+        this.ui = ui;
+        this.backGroundPanel = ui.bgPanel;
+        this.font = ui.eastSeaDokdo;
+
+//        backGroundPanel.setLayout(null);
+//        backGroundPanel.setBounds(0, 0, 80, 60);
+//        backGroundPanel.setBackground(new Color(150, 150, 150));
+
+        createCounterLabel();
+
+        backGroundPanel.add(counterLabel);
+        backGroundPanel.add(backGroundPanel);
+
+        second = 0;
+        minute = 0;
+        simpleTimer();
+        timer.start(); // kinda like background operation hoye jay
+
+    }
+
+    public void createCounterLabel(){
+        counterLabel = new JLabel();
+        counterLabel.setBounds(5,5, 100, 50);
+        counterLabel.setFont(font);
+        counterLabel.setText("00:00");
+
+        backGroundPanel.add(counterLabel);
+        backGroundPanel.add(backGroundPanel);
+    }
+
+    public void simpleTimer() {
+
+        timer = new Timer(1000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                second ++;
+                if (second >= 60)
+                {
+                    minute ++;
+                    second -= 60;
+                }
+                //drawTimer();
+            }
+
+        }); // updates every 1 second
+    }
+
+    public String drawTimer() {
+        if(minute<10 && second<10) {
+            return ("0" + minute + ":" + "0" + second);
+        }
+        else if (minute<10 && second>=10) {
+            return ("0" + minute + ":" + second);
+        }
+        else if (minute>=10 && second<10) {
+            return ( minute + ":" + "0" + second);
+        }
+        else {
+            return (minute + ":" + second);
+        }
+    }
+}
