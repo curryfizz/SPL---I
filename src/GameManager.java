1cadb834f7f532616172447d859102472a6c8999
curryfizz <nawsheen141@gmail.com>
2022-10-11 02:31:59 +0600

    DecoyAnimation edits

--- src/GameManager.java
+++ src/GameManager.java
@@ -12,15 +12,15 @@ public class GameManager {
 
     public GameManager(){
 //        GameTimer gameTimer = new GameTimer(ui);
-//        animationTest = new AnimationTest(10);
 
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        jFrame.setBackground(Color.decode("#14171C"));
         jFrame.setUndecorated(true);
         jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        DecoyAnimation decoy = new DecoyAnimation(jFrame,7);
 //        jFrame.add(animationTest);
-        StartMenu startMenu = new StartMenu(jFrame);
+//        StartMenu startMenu = new StartMenu(jFrame);
         jFrame.pack();
         jFrame.setVisible(true);
     }
