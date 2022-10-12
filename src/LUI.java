278cb7f5cc722685c0aad1c17ba1afc9d47fc228
lomatul <90206489+lomatul@users.noreply.github.com>
2022-10-12 21:13:26 +0600

    with Text

--- src/LUI.java
+++ src/LUI.java
@@ -14,14 +14,14 @@ public class LUI {
     Rectangle maxBounds;
     GraphicsEnvironment ge;
     JFrame window ,textbx ;
-    public  TextArea messageText;
-    private JButton button;
+
     public  JPanel bgPanel[] = new JPanel[10];
     public  JLabel bgLabel[] = new JLabel[10];
     public  JLabel objLabel[]= new JLabel[23];
 
     public ArrayList<JLabel> imagelist = new ArrayList<>();
     public ArrayList<ObjectHidingButton> buttonlist = new ArrayList<>();
+    public ArrayList<String> Textlist = new ArrayList<>();
 
 
     public  JComponent obj[]=new  JComponent[10];
@@ -191,8 +191,6 @@ public class LUI {
         JLabel objtLabel = new JLabel();
         objtLabel.setBounds(0,0,maxBounds.width,maxBounds.height);
 
-
-
         ImageIcon  obj1icon= new ImageIcon(getClass().getClassLoader().getResource(image));
         Image image1 = obj1icon.getImage();
         image1 = image1.getScaledInstance(maxBounds.width, maxBounds.height-100, Image.SCALE_DEFAULT);
@@ -204,10 +202,18 @@ public class LUI {
 
     }
 
+    public void createText (String text)
+    {
+        Textlist.add(text);
+
+
+    }
+
 
     public void  createButton(String image,int posx, int posy, int sizex,int sizey)
     {
         createObject(image);
+
         ObjectHidingButton objectHidingButton = new ObjectHidingButton(posx,posy,sizex,sizey,imagelist.get(imagelist.size()-1));
         //imagelist.get(imagelist.size-1)
         bgPanel[1].add(objectHidingButton);
@@ -221,27 +227,49 @@ public class LUI {
     {
 
         createButton("images/01.png",308,450,65,84);
+        createText("Cornflakes Box");
         createButton("images/02.png",1190,520,18,30);
+        createText("CocaCola Can");
         createButton("images/03.png",1242,440,78,54);
+        createText("ShoulderBag");
         createButton("images/04.png",1130,750,60,26);
+        createText("HeadPhone");
         createButton("images/05.png",330,740,43,32);
+        createText("Phone");
         createButton("images/06.png",410,689,69,13);
+        createText("Phone");
         createButton("images/07.png",1104,712,68,22);
+        createText("Sunglasses");
         createButton("images/08.png",860,410,25,12);
+        createText("Garbage");
         createButton("images/09.png",484,390,50,32);
+        createText("Toilet Paper");
         createButton("images/10.png",544,440,68,24);
+        createText("Food");
         createButton("images/11.png",94,750,158,24);
+        createText("Cloth");
         createButton("images/12.png",744,555,98,60);
+        createText("BagPack");
         createButton("images/13.png",523,610,34,68);
+        createText("Scarf");
         createButton("images/14.png",184,470,80,80);
+        createText("Pizza Box");
         createButton("images/15.png",1034,440,28,20);
+        createText("Folded Clothes");
         createButton("images/16.png",894,524,28,19);
+        createText("Chips Packet");
         createButton("images/17.png",1204,730,98,43);
+        createText("Unfolded Cloth");
         createButton("images/18.png",724,50,160,20);
+        createText("Quilt");
         createButton("images/19.png",924,524,30,19);
+        createText("Book");
         createButton("images/20.png",1368,760,48,14);
+        createText("FoodPlate");
         createButton("images/21.png",934,485,40,24);
+        createText("Shoes");
         createButton("images/22.png",494,435,18,14);
+        createText("TeaCup");
 
 
 
