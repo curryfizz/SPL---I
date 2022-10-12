463cfc88102fb54fb6e83437e8a582bae1f31a42
lomatul <90206489+lomatul@users.noreply.github.com>
2022-10-12 15:15:28 +0600

    transparent button background

--- src/ObjectHidingButton.java
+++ src/ObjectHidingButton.java
@@ -17,15 +17,18 @@ public class ObjectHidingButton extends JButton {
         this.buttonHeight = height;
         this.toggleLabel = label;
         createInvisibleButton();
+
     }
 
 
     public void createInvisibleButton(){
+        repaint();
         setBackground(new Color(0,0,0,0));
-        setBackground(new Color(255,0,0,50));
+        setBackground(new Color(0,0,0,0));
+        repaint();
         setBounds(positionX,positionY,buttonWidth,buttonHeight);
         setFocusPainted(false);
-//      setContentAreaFilled(false);
+        setContentAreaFilled(false);
         setBorderPainted(false);
         addMouseListener(new SceneObjectEvents(toggleLabel));
 
