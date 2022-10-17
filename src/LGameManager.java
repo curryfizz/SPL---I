f56705faac87d695b8352c72cce144c7a3f0f999
lomatul <90206489+lomatul@users.noreply.github.com>
2022-10-18 01:34:53 +0600

    slider done in main class but not added to actual screen

--- src/LGameManager.java
+++ src/LGameManager.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 
@@ -32,60 +34,72 @@ public class LGameManager {
    }
     public  LGameManager(){
         JFrame jFrame = new JFrame();
+
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setUndecorated(true);
         jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         jFrame.setVisible(true);
         jFrame.setBackground(Color.white);
 
-                URL url = getClass().getResource("D:\\SPL---I\\images\\bgmusic.wav");
-        playmusic(url);
+        jFrame.setLayout(new GridLayout(1,3));
+
+        JButton volumeUpB = new JButton("Volume UP");
+        volumeUpB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sound.volumeUp();
+            }
+        });
+
+
 
         DeviceScreenInformation deviceScreenInformation = new DeviceScreenInformation();
         FontInfo fontInfo = new FontInfo();
         DormRoomScene dormRoomScene = new DormRoomScene(jFrame,deviceScreenInformation,fontInfo);
+
         MusicSlider musicSlider = new MusicSlider(jFrame,dormRoomScene, deviceScreenInformation,fontInfo);
- musicSlider.addChangeListener(new ChangeListener() {
-     @Override
-     public void stateChanged(ChangeEvent e) {
-         sound.currentVolume = musicSlider.getValue();
-         sound.fc.setValue(sound.currentVolume);
-     }
- });
+
+        jFrame.add(volumeUpB);
+
+       // slider = new JSlider(-80,7);
+        musicSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                sound.currentVolume = slider.getValue();
+                sound.fc.setValue(sound.currentVolume);
+            }
+        });
+        jFrame.add(musicSlider);
+        jFrame.pack();
+        jFrame.setVisible(true);
 
 
+        URL soundURl = getClass().getResource("/images/bgmusic.wav");
 
-
-
+        playMusic(soundURl);
     }
-
-
-
-    public void playmusic(URL url)
+    public void playMusic(URL url)
     {
-sound.setFile(url);
-sound.play(url);
-sound.loop(url);
+        sound.setFile(url);
+        sound.play(url);
     }
-
-
-
-
-
-
-
-//    public  void playSE(URL url)
-//    {
-//        se.setFile(url);
-//        se.play(url);
-//    }
-
-  /*  public void stopMusic(URL url)
-    {
-        MusicPlayer.stop(url);
-    }
-
-   */
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,59 @@
+package src;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+import java.net.URL;
+
+public class Main {
+    JSlider slider;
+    Sound sound = new Sound();
+    public  static void main(String[] args){
+        new Main();
+    }
+    public  Main(){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new GridLayout(1,3));
+
+        JButton volumeUpB = new JButton("Volume UP");
+        volumeUpB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sound.volumeUp();
+            }
+        });
+        window.add(volumeUpB);
+        slider = new JSlider(-40,7);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                sound.currentVolume = slider.getValue();
+                if(sound.currentVolume == -40){
+                    sound.currentVolume = -80;
+                }
+                sound.fc.setValue(sound.currentVolume);
+            }
+        });
+        JPanel dummy = new JPanel();
+        dummy.setBounds(new DeviceScreenInformation().graphicsEnvironment.getMaximumWindowBounds());
+        dummy.setBackground(Color.BLACK);
+        dummy.add(slider);
+        window.add(dummy);
+        window.pack();
+        window.setVisible(true);
+
+        URL soundURl = getClass().getResource("/images/bgmusic.wav");
+
+        playMusic(soundURl);
+    }
+    public void playMusic(URL url)
+    {
+        sound.setFile(url);
+        sound.play(url);
+    }
+}
