3b7818ea1f300b42e984c5226e63c1f0430b8dd5
curryfizz <nawsheen141@gmail.com>
2022-11-10 13:07:49 +0600

    added some methods to parent class

--- src/levels/ALevelPanel.java
+++ src/levels/ALevelPanel.java
@@ -1,6 +1,5 @@
 package src.levels;
 
-import src.*;
 import src.buttons.ObjectHidingButton;
 import src.levelObjects.ScoreBoard;
 import src.levelObjects.TimerLabel;
@@ -10,17 +9,15 @@ import src.transitionPanels.MapT;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public abstract class ALevelPanel extends JPanel {
+public abstract class ALevelPanel extends JPanel implements Runnable{
     public TimerLabel timerLabel;
     ScoreBoard scoreBoard;
     public JLabel ShowGottenScore;
     public JLabel HintAnimationGif;
-
-    Main main;
-
     JLabel congratulationsConfetti;
     public ArrayList<JLabel> imageList = new ArrayList<>(); // array of images of items we may need to find
     public ArrayList<ObjectHidingButton> buttonList = new ArrayList<>(); //all the buttons for the objects are in this
@@ -31,7 +28,7 @@ public abstract class ALevelPanel extends JPanel {
     public void createConfettiScreen(){
         congratulationsConfetti = new JLabel();
         congratulationsConfetti.setBounds(0,0, DeviceInformation.screenWidth,DeviceInformation.screenHeight);
-        ImageIcon gif = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("images/congratulations/friendsclapping.gif")));
+        ImageIcon gif = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("images/congratulations/levelFinishedSuccessfullyConfetti.gif")));
         gif.setImage(gif.getImage().getScaledInstance(DeviceInformation.screenWidth, DeviceInformation.screenHeight, Image.SCALE_DEFAULT));
         congratulationsConfetti.setIcon(gif);
         congratulationsConfetti.setVisible(false);
@@ -40,6 +37,18 @@ public abstract class ALevelPanel extends JPanel {
     public abstract void startScene(); //when clicked from Map, resets some things and starts the scene
     public abstract void PrepareForSceneTransition(LoadingAnimationT loadingAnimationT, MapT mapT); // scene transition to mapT
     public abstract  void EndLevel();
+    public abstract void buildScene() throws IOException;
+
+    @Override
+    public void run(){
+        try {
+            buildScene();
+
+        }catch (IOException e){
+
+        }
+    }
+
 
 
 }
