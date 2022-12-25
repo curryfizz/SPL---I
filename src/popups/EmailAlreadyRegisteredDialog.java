ce258223c4b847afc6650fb1c497f7f46039d740
curryfizz <nawsheen141@gmail.com>
2022-12-26 02:23:21 +0600

    SignupDialog email already in database dialog

---
+++ src/popups/EmailAlreadyRegisteredDialog.java
@@ -0,0 +1,51 @@
+package src.popups;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EmailAlreadyRegisteredDialog extends AccountNotSuccessDialog {
+    public EmailAlreadyRegisteredDialog(AccountDialog accountDialog) {
+        super(accountDialog);
+        addUnsuccessfulReasonLabel("Email already registered!");
+        addButtonFieldLabel();
+        addYesButton("Login");
+        addNoButton("Try Again!");
+        setVisible(true);
+
+    }
+
+    @Override
+    protected void addYesButtonMouseEvents() {
+        yesButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+                parentAccountDialog.dispose();
+                LoginDialog loginDialog = new LoginDialog(parentAccountDialog.jFrame, parentAccountDialog.database);
+                parentAccountDialog.jFrame.repaint();
+                parentAccountDialog.jFrame.revalidate();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+}
