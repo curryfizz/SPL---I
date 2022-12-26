293d9560e1185fc62f7ceef1e932902597a9dbfc
curryfizz <nawsheen141@gmail.com>
2022-12-27 02:02:49 +0600

    created labels for start menu

---
+++ src/levelObjects/StartMenuLabel.java
@@ -0,0 +1,34 @@
+package src.levelObjects;
+
+import src.setup.DeviceInformation;
+import src.setup.FontInfo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartMenuLabel extends JLabel {
+
+
+    public StartMenuLabel(int width, int height){
+//        addLabelStyles(width,height);
+    }
+
+    public StartMenuLabel(){
+        addLabelStyles();
+    }
+
+    private void addLabelStyles(){
+        setFont(FontInfo.getResizedFont((float)0.09*(DeviceInformation.screenWidth)));
+        setText("LOST TREASURES");
+        setHorizontalAlignment(JLabel.CENTER);
+        setForeground(Color.white);
+        setBackground(Color.decode("#14171C"));
+    }
+
+    @Override
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D)g;
+
+
+    }
+}
