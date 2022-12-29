5b844dde9e874a82b05196af6cd2d69abba83c3a
curryfizz <nawsheen141@gmail.com>
2022-12-29 20:08:11 +0600

    added final animation gif label and added restart game button

--- src/popups/FinalLevelCompletedPopUp.java
+++ src/popups/FinalLevelCompletedPopUp.java
@@ -8,34 +8,46 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Objects;
 
 public class FinalLevelCompletedPopUp extends JDialog {
-    JLabel jLabel;
+    JLabel animationGifLabel;
     JButton closeButton;
+    JButton restartGameButton;
 
+    JLabel congratulationsLabel;
+    JLabel completedGameLabel;
 
     public FinalLevelCompletedPopUp(JFrame jFrame, int level_number)
     {
-
         setModal(true);
         setUndecorated(true);
-
         getContentPane().setBackground(Color.decode("#14171C"));
         setLayout(new FlowLayout());
         getRootPane().setBorder(new LineBorder(Color.white,2));
-        jLabel = new JLabel();
-
-        jLabel.setPreferredSize(new Dimension(300,200));
-        jLabel.setLayout(new FlowLayout());
-        jLabel.setForeground(Color.white);
-        jLabel.setText("GAME FINISHED");
-        jLabel.setFont(FontInfo.getResizedFont(43f));
-        add(jLabel);
-        jLabel.setHorizontalTextPosition(JLabel.CENTER);
-        setSize(320,280);
+        addAnimationGifLabel();
+        addCongratulationsLabel();
+        addCompletedGameLevel();
+        setSize(700,500);
         setLocationRelativeTo(jFrame);
         setResizable(false);
+        addQuitButton();
+        setVisible(true);
+    }
 
+    private void addAnimationGifLabel(){
+        animationGifLabel = new JLabel();
+        animationGifLabel.setPreferredSize(new Dimension(300,300));
+        animationGifLabel.setLayout(null);
+        animationGifLabel.setHorizontalAlignment(JLabel.CENTER);
+        ImageIcon animationGifIcon = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("images/finishedgameanimationv3.gif")));
+        animationGifIcon.setImage(animationGifIcon.getImage().getScaledInstance((int) (animationGifIcon.getIconWidth()*0.625), (int) (animationGifIcon.getIconHeight()*0.625), Image.SCALE_DEFAULT));
+        animationGifLabel.setIcon(animationGifIcon);
+        animationGifLabel.setBackground(null);
+        repaint();
+        add(animationGifLabel);
+    }
+    private void addQuitButton() {
         closeButton = new JButton();
         closeButton.setBackground(Color.decode("#14171C"));
         closeButton.setPreferredSize(new Dimension(210,40));
@@ -45,40 +57,133 @@ public class FinalLevelCompletedPopUp extends JDialog {
         closeButton.setForeground(Color.white);
         closeButton.setFont(FontInfo.getResizedFont(28f));
         closeButton.setOpaque(true);
-        closeButton.setText("Okay");
+        closeButton.setFocusPainted(false);
+        closeButton.setText("Quit");
         closeButton.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
                 Sound alert = new Sound();
                 alert.setFile("audio/soundeffects/alert.wav");
                 alert.play();
-                dispose();
+                System.exit(0);
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-
+                repaint();
+                closeButton.setForeground(Color.decode("#14171C"));
+                closeButton.setBackground(Color.white);
+                repaint();
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
+                Sound alert = new Sound();
+                alert.setFile("audio/soundeffects/alert.wav");
+                alert.play();
+                System.exit(0);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                repaint();
+                closeButton.setForeground(Color.decode("#14171C"));
+                closeButton.setBackground(Color.white);
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                repaint();
+                closeButton.setBackground(Color.decode("#14171C"));
+                closeButton.setForeground(Color.white);
+                repaint();
+            }
+        });
+        add(closeButton);
+    }
+    private void addRestartGameButton() {
+        restartGameButton = new JButton();
+        restartGameButton.setBackground(Color.decode("#14171C"));
+        restartGameButton.setPreferredSize(new Dimension(210,40));
+        restartGameButton.setFocusPainted(false);
+        restartGameButton.setHorizontalAlignment(JButton.CENTER);
+        restartGameButton.setBorder(new LineBorder(Color.white,3));
+        restartGameButton.setForeground(Color.white);
+        restartGameButton.setFont(FontInfo.getResizedFont(28f));
+        restartGameButton.setOpaque(true);
+        restartGameButton.setFocusPainted(false);
+        restartGameButton.setText("Restart Game");
+        restartGameButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Sound alert = new Sound();
+                alert.setFile("audio/soundeffects/alert.wav");
+                alert.play();
+                System.exit(0);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                repaint();
+                restartGameButton.setForeground(Color.decode("#14171C"));
+                restartGameButton.setBackground(Color.white);
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                Sound alert = new Sound();
+                alert.setFile("audio/soundeffects/alert.wav");
+                alert.play();
+                System.exit(0);
 
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-
+                repaint();
+                restartGameButton.setForeground(Color.decode("#14171C"));
+                restartGameButton.setBackground(Color.white);
+                repaint();
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-
+                repaint();
+                restartGameButton.setBackground(Color.decode("#14171C"));
+                restartGameButton.setForeground(Color.white);
+                repaint();
             }
         });
-        add(closeButton);
-        setVisible(true);
+        add(restartGameButton);
     }
+
+    private void addCongratulationsLabel() {
+        congratulationsLabel = new JLabel();
+        congratulationsLabel.setPreferredSize(new Dimension(600,50));
+        congratulationsLabel.setLayout(new FlowLayout());
+        congratulationsLabel.setForeground(Color.white);
+        congratulationsLabel.setText("Congratulations!");
+        congratulationsLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        congratulationsLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        congratulationsLabel.setFont(FontInfo.getResizedFont(43f));
+        congratulationsLabel.setHorizontalTextPosition(JLabel.CENTER);
+        add(congratulationsLabel);
+    }
+        private void addCompletedGameLevel() {
+        completedGameLabel = new JLabel();
+        completedGameLabel.setPreferredSize(new Dimension(600,50));
+        completedGameLabel.setLayout(new FlowLayout());
+        completedGameLabel.setForeground(Color.white);
+        completedGameLabel.setText("You've completed the game!");
+        completedGameLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        completedGameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        completedGameLabel.setFont(FontInfo.getResizedFont(43f));
+        completedGameLabel.setHorizontalTextPosition(JLabel.CENTER);
+        add(completedGameLabel);
+    }
+
     public static String convertToMultiline(String orig)
     {
         return "<html>" + orig.replaceAll("\n", "<br>");
